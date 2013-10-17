b77a4dcbf400a834c6e2e3e87f4fb0d75e613eb9
Arjit Chaudhary <arjit.c@gmail.com>
2013-10-17 16:05:45 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo Starting Kloxo-MR install...
+read -p "press [enter] to start..."
+cd /
+yum update -y
+yum install yum-utils yum-priorities vim-minimal subversion curl zip unzip -y
+yum install telnet -y
+setenforce 0
+echo ‘SELINUX=disabled’ > /etc/selinux/config
+cd /etc/yum.repos.d/
+wget https://github.com/mustafaramadhan/kloxo/raw/release/kloxo-mr.repo --no-check-certificate
+cd /
+yum install kloxomr -y
