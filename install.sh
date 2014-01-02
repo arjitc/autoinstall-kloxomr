156edaf979d3daa8e6086112f8d3365f0d3a1ac6
Arjit Chaudhary <arjit.c@gmail.com>
2014-01-02 16:58:55 +0530

    Fixed.

--- install.sh
+++ install.sh
@@ -7,12 +7,14 @@ yum install yum-utils yum-priorities vim-minimal subversion curl zip unzip -y
 yum install telnet -y
 setenforce 0
 echo ‘SELINUX=disabled’ > /etc/selinux/config
-cd /etc/yum.repos.d/
-rm -rf /tmp/kloxo; mkdir /tmp/kloxo ; cd /tmp/kloxo; 
-rm -rf kloxo-mr.repo
 wget https://github.com/mustafaramadhan/kloxo/raw/rpms/release/neutral/noarch/mratwork-release-0.0.1-1.noarch.rpm --no-check-certificate
 rpm -ivh mratwork-release-0.0.1-1.noarch.rpm
 yum update mratwork-release
+cd /
+yum clean all
+yum install yum-plugin-replace -y
+yum replace mysql --replace-with=mysql55 -y
+chkconfig mysqld on
 yum install kloxomr -y
 sh /script/upcp
 read -p "Install done! Reboot VPS once done!"
