e1171d893d60fe240d30c3fbf91df67451c43af6
Arjit Chaudhary <arjit.c@gmail.com>
2014-01-02 17:03:22 +0530

    Fixed again

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ yum install telnet -y
 setenforce 0
 echo ‘SELINUX=disabled’ > /etc/selinux/config
 wget https://github.com/mustafaramadhan/kloxo/raw/rpms/release/neutral/noarch/mratwork-release-0.0.1-1.noarch.rpm --no-check-certificate
+wget https://raw.github.com/arjitc/autoinstall-kloxomr/master/mratwork.repo -O /etc/yum.repos.d/mratwork.repo
 rpm -ivh mratwork-release-0.0.1-1.noarch.rpm
 yum update mratwork-release
 cd /
@@ -16,5 +17,8 @@ yum install yum-plugin-replace -y
 yum replace mysql --replace-with=mysql55 -y
 chkconfig mysqld on
 yum install kloxomr -y
+sh /script/upcp
+yum clean all
+yum reinstall kloxomr
 sh /script/upcp
 read -p "Install done! Reboot VPS once done!"
