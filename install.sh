d7710d05ee31e22dff120530a2ff914010870dfb
Arjit Chaudhary <arjit.c@gmail.com>
2014-01-02 16:54:12 +0530

    Fixed.

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@ echo ‘SELINUX=disabled’ > /etc/selinux/config
 cd /etc/yum.repos.d/
 rm -rf /tmp/kloxo; mkdir /tmp/kloxo ; cd /tmp/kloxo; 
 rm -rf kloxo-mr.repo
-wget https://github.com/mustafaramadhan/kloxo/raw/dev/kloxo/install/kloxo-mr-dev.sh --no-check-certificate
-sh ./kloxo-mr-dev.sh
+wget https://github.com/mustafaramadhan/kloxo/raw/rpms/release/neutral/noarch/mratwork-release-0.0.1-1.noarch.rpm --no-check-certificate
+rpm -ivh mratwork-release-0.0.1-1.noarch.rpm
+yum update mratwork-release
+yum install kloxomr -y
+sh /script/upcp
 read -p "Install done! Reboot VPS once done!"
