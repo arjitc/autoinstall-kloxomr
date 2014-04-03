ef5766e2f3d21e2fd2f17d312f06690ab23efd5c
Arjit Chaudhary <arjit.c@gmail.com>
2014-04-03 21:03:15 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,21 +7,30 @@ yum install yum-utils yum-priorities vim-minimal subversion curl zip unzip -y
 yum install telnet -y
 setenforce 0
 echo ‘SELINUX=disabled’ > /etc/selinux/config
-rpm -ivh https://github.com/mustafaramadhan/kloxo/raw/rpms/release/neutral/noarch/mratwork-release-0.0.1-1.noarch.rpm
-yum update mratwork-release -y
+ # move repo dir
+    cd /etc/yum.repos.d/
+
+    # get repo file -- no need for 6.5.0.c and after
+    wget https://github.com/mustafaramadhan/kloxo/raw/release/kloxo-mr.repo --no-check-certificate
+
+    # move to /
+    cd /
+
+    # install (and then run 'setup.sh' instead 'installer.sh')
+    yum install kloxomr
 #yum install yum-plugin-replace -y
 #yum replace mysql --replace-with=mysql55 -y
 #chkconfig mysqld on
 #yum install kloxomr -y
     # to make sure uss latest version of OS
-    yum clean all
-    yum update -y
-    sh /script/convert-to-qmailtoaster
+    #yum clean all
+    #yum update -y
+    #sh /script/convert-to-qmailtoaster
     
     # install
-    yum install kloxomr -y
-    sh /script/upcp -y
+    #yum install kloxomr -y
+    #sh /script/upcp -y
 #yum reinstall kloxomr
 #sh /script/upcp
-sh /script/restart
+#sh /script/restart
 read -p "Install done! Reboot VPS once done!"
